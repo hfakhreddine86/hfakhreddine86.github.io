a26018926d8eae7b7461fc28fcd90e86f805f66a
Hussein Fakhreddine <hfakhreddine86@gmail.com>
2018-02-22 23:00:21 -0800

    updated layout and added projects

--- js/init.js
+++ js/init.js
@@ -1,8 +1,49 @@
-(function($){
-  $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function () {
+
+  // handle links with @href started with '#' only
+  $(document).on('click', 'a[href^="#"]', function (e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+      return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({ scrollTop: pos });
+  });
+
+  (function ($) {
+    $(function () {
+
+      $('.button-collapse').sideNav({
+        closeOnClick: true
+      }
+      );
+      $('.parallax').parallax();
+
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () { scrollFunction() };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("myBtn").style.display = "block";
+    } else {
+      document.getElementById("myBtn").style.display = "none";
+    }
+  }
+
+})
